cc29cf2e2735156facf95a1efd9134f101a65c3e
Xetoris <cmccord4pa@gmail.com>
2018-12-17 16:43:44 -0500

    Adding regression-app responses, removed worrisome data.

---
+++ web/src/app/app-services/regression-app/mock.regression-app.responses.ts
@@ -0,0 +1,10 @@
+/**
+ * Contains a collection of mock responses taken from the API directly.
+ */
+export const MockRegressionAppResponses = {
+  regressionResults: {
+    totalRecords: 10,
+    maxScore: null,
+    records: []
+  }
+};
