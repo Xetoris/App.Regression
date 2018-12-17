3c55b2b620a39cdb6294a790a538f684606eb296
Xetoris <cmccord4pa@gmail.com>
2018-12-17 16:48:04 -0500

    Added an example to the result response.

--- web/src/app/app-services/regression-app/mock.regression-app.responses.ts
+++ web/src/app/app-services/regression-app/mock.regression-app.responses.ts
@@ -5,6 +5,32 @@ export const MockRegressionAppResponses = {
   regressionResults: {
     totalRecords: 10,
     maxScore: null,
-    records: []
+    records: [{
+      auditId: '3959e9a4-7f78-4906-a173-efc6a0bb50b3',
+      buildId: '1',
+      buildName: 'Test.Example',
+      buildUrl: '',
+      duration: 98,
+      environment: 'UAT',
+      exceptionLine: '114',
+      exceptionMessage: '500 Internal Server Error',
+      exceptionSource: '/usr/share/jenkins/workspace/TestExamples/features/step_definitions/test_steps',
+      group: 'Testing',
+      id: 'AWTdq0F56cdJNc297zyW',
+      index: 'tests',
+      machine: 'SomeMachine',
+      medium: 'cucumber',
+      score: null,
+      source: 'features/tests/tests.feature:1',
+      status: 'failed',
+      tags: [
+        '@test',
+        '@example'
+      ],
+      test: 'This is an example test.',
+      type: 'result',
+      watchUrl: '',
+      date: '2018-12-17T21:00:00Z'
+    }]
   }
 };
