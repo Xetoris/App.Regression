c9e5b4670241066b2415bc34523cc017f6622f43
Xetoris <cmccord4pa@gmail.com>
2018-12-18 15:41:07 -0500

    Adding some more example responses.

--- web/src/app/app-services/regression-app/mock.regression-app.responses.ts
+++ web/src/app/app-services/regression-app/mock.regression-app.responses.ts
@@ -5,32 +5,115 @@ export const MockRegressionAppResponses = {
   regressionResults: {
     totalRecords: 10,
     maxScore: null,
-    records: [{
-      auditId: '3959e9a4-7f78-4906-a173-efc6a0bb50b3',
-      buildId: '1',
-      buildName: 'Test.Example',
-      buildUrl: '',
-      duration: 98,
-      environment: 'UAT',
-      exceptionLine: '114',
-      exceptionMessage: '500 Internal Server Error',
-      exceptionSource: '/usr/share/jenkins/workspace/TestExamples/features/step_definitions/test_steps',
-      group: 'Testing',
-      id: 'AWTdq0F56cdJNc297zyW',
-      index: 'tests',
-      machine: 'SomeMachine',
-      medium: 'cucumber',
-      score: null,
-      source: 'features/tests/tests.feature:1',
-      status: 'failed',
-      tags: [
-        '@test',
-        '@example'
-      ],
-      test: 'This is an example test.',
-      type: 'result',
-      watchUrl: '',
-      date: '2018-12-17T21:00:00Z'
-    }]
+    records: [
+      {
+        auditId: '3959e9a4-7f78-4906-a173-efc6a0bb50b3',
+        buildId: '1',
+        buildName: 'Test.Example',
+        buildUrl: '',
+        duration: 98,
+        environment: 'UAT',
+        exceptionLine: '114',
+        exceptionMessage: '500 Internal Server Error',
+        exceptionSource: '/usr/share/jenkins/workspace/TestExamples/features/step_definitions/test_steps',
+        group: 'Testing',
+        id: 'AWTdq0F56cdJNc297zyW',
+        index: 'tests',
+        machine: 'SomeMachine',
+        medium: 'cucumber',
+        score: null,
+        source: 'features/tests/tests.feature:1',
+        status: 'failed',
+        tags: [
+          '@test',
+          '@example'
+        ],
+        test: 'This is an example test.',
+        type: 'result',
+        watchUrl: '',
+        date: '2018-12-17T21:00:00Z'
+      },
+      {
+        auditId: 'db80edf3-01a5-4e2f-acee-74a6d8e38eed',
+        buildId: '1',
+        buildName: 'Test.Example',
+        buildUrl: '',
+        duration: 114,
+        environment: 'UAT',
+        exceptionLine: null,
+        exceptionMessage: null,
+        exceptionSource: null,
+        group: 'Testing',
+        id: 'AWTdqzUQ780xQQsPh9Wj',
+        index: 'tests',
+        machine: 'SomeMachine',
+        medium: 'cucumber',
+        score: null,
+        source: 'features/tests/tests.feature:148',
+        status: 'passed',
+        tags: [
+          '@test',
+          '@example'
+        ],
+        test: 'This is an example test.',
+        type: 'result',
+        watchUrl: null,
+        date: '2018-12-17T21:35:42Z'
+      },
+      {
+        auditId: 'a4f092b6-586e-4105-89b6-f4195bd0fcac',
+        buildId: '1',
+        buildName: 'Test.Example',
+        buildUrl: '',
+        duration: 134,
+        environment: 'UAT',
+        exceptionLine: null,
+        exceptionMessage: null,
+        exceptionSource: null,
+        group: 'Testing',
+        id: 'AWTdqwp4780xQQsPh9Wi',
+        index: 'tests',
+        machine: 'SomeMachine',
+        medium: 'cucumber',
+        score: null,
+        source: 'features/tests/tests.feature:35',
+        status: 'passed',
+        tags: [
+          '@test',
+          '@example'
+        ],
+        test: 'This is an example test.',
+        type: 'result',
+        watchUrl: '',
+        date: '2018-12-17T21:35:31Z'
+      },
+      {
+        auditId: '0463f592-ae35-4d8d-bc32-2b5525bb0579',
+        buildId: '1',
+        buildName: 'Test.Example',
+        buildUrl: '',
+        duration: 125,
+        environment: 'UAT',
+        exceptionLine: null,
+        exceptionMessage: null,
+        exceptionSource: null,
+        group: 'Testing',
+        id: 'AWTdqwPX780xQQsPh9Wh',
+        index: 'tests',
+        machine: 'SomeMachine',
+        medium: 'cucumber',
+        score: null,
+        source: 'features/tests/tests.feature:120',
+        status: 'passed',
+        tags: [
+          '@test',
+          '@example'
+        ],
+        test: 'This is an example test.',
+        type: 'result',
+        watchUrl: '',
+        date: '2018-12-17T21:35:29Z'
+      }
+    ]
   }
 };
